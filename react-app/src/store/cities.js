b450e58c429ad3ead25d4727f24be18ef156690d
Chad Calcote <calcote@att.net>
2021-03-11 16:32:10 -0700

    Comment City Reducer

--- react-app/src/store/cities.js
+++ react-app/src/store/cities.js
@@ -1,5 +1,7 @@
+// Action Type
 const SET_ONE_CITY = "SET_ONE_CITY"
 
+// Action - Set One City to the store
 export const setOneCity = (city) => {
   return {
     type: SET_ONE_CITY,
@@ -7,6 +9,11 @@ export const setOneCity = (city) => {
   };
 };
 
+// Action Creator (Thunk)
+// Takes Data (city) and dispatches inside itself - dispatching eventually to reducer
+// Thunk allows us to deal with asynchronous calls
+// This fetches once city from API route based on input
+// Then dispatches that action to the reducer
 export const fetchOneCity = (city) => {
   return async (dispatch) => {
     const responseFromDb = await fetch(`/api/cities/${city}`);
@@ -14,9 +21,9 @@ export const fetchOneCity = (city) => {
     dispatch(setOneCity(found_city));
   };
 };
-
+// Setup initial state
 const initialState = [];
-
+// All city information in store lives here and is sent up to the root reducer
 const citiesReducer = (state = initialState, action) => {
   let newState;
   switch (action.type) {
