01c61d8295b77611390395b71065a4f7de7f72ec
Chad Calcote <calcote@att.net>
2021-03-11 14:48:04 -0700

    Comment Map Container

--- react-app/src/components/MapContainer/index.js
+++ react-app/src/components/MapContainer/index.js
@@ -1,22 +1,28 @@
+// React Dependencies
 import React, { useState } from "react";
+// Google Maps API Dependencies
 import { GoogleMap, LoadScript, Marker, InfoWindow } from "@react-google-maps/api";
 import googleMapsApiKey from "../../apiKey";
 
-const MapContainer = ({restaurants, city}) => {
+// Define component with destructured props
+const MapContainer = ({ restaurants, city }) => {
 
+    // React Hooks
+    // Setup state of selected -- this wil display the selected restaurant location
     const [selected, setSelected] = useState({});
-
-    const onSelect = item => {
-      setSelected(item)
+    // Component Functions / Variables
+    // Once you select a restaurant location on map, we wil set the state to that restaurant
+    const onSelect = restaurant => {
+      setSelected(restaurant)
     };
-
+    // Styles for map
     const mapStyles = {
       height: "200px",
       width: "300px",
       borderRadius: "20px",
       boxShadow: "5px 0 5px -2px #036603",
     };
-
+    // Default Point on map where it will display
     const defaultCenter = {
       lat: parseFloat(city["lat"]),
       lng: parseFloat(city["lng"])
