9c142f0e01c2b42c41c2677064fa491beef0590b
Chad Calcote <calcote@att.net>
2021-03-11 14:12:11 -0700

    Change file name to be uniform

--- react-app/src/components/CityPage/index.js
+++ react-app/src/components/CityPage/index.js
@@ -1,4 +1,4 @@
-import "./city.css";
+import "./index.css";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
