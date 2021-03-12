d25650b36f7005136f6994d38305321750e65711
Chad Calcote <calcote@att.net>
2021-03-12 05:28:28 -0700

    Format Models

--- app/models/city.py
+++ app/models/city.py
@@ -2,22 +2,24 @@
 from .db import db
 
 # Setup City Model
+
+
 class City(db.Model):
-  __tablename__ = 'cities'
+    __tablename__ = 'cities'
 
-  # Identify Columns for Tables with any constraints
-  id = db.Column(db.Integer, primary_key = True)
-  name = db.Column(db.String(40), nullable = False, unique = True)
-  photo = db.Column(db.Text, nullable = False)
-  lng = db.Column(db.String, nullable = False)
-  lat = db.Column(db.String, nullable = False)
+    # Identify Columns for Tables with any constraints
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False, unique=True)
+    photo = db.Column(db.Text, nullable=False)
+    lng = db.Column(db.String, nullable=False)
+    lat = db.Column(db.String, nullable=False)
 
-  # Return instances of City class as dictionaries
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "name": self.name,
-      "photo": self.photo,
-      "lng": self.lng,
-      "lat": self.lat
-    }
+    # Return instances of City class as dictionaries
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "photo": self.photo,
+            "lng": self.lng,
+            "lat": self.lat
+        }
